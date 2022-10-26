f091116d08610c5f46671f50c90de6eedef1202a
Mu-X-Yun <111125488+Mu-X-Yun@users.noreply.github.com>
2022-10-26 14:40:01 +0800

    Update Number Guessing Game.c

--- Number Guessing Game.c
+++ Number Guessing Game.c
@@ -1,17 +1,20 @@
 //小游戏：猜数字
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 void main()
 {
 	int n = 1;
+	srand((unsigned int)time(NULL));
 	while (n == 1)
 	{
 		int i = 0, a = 0;
 		printf("\n***** Numnber Guessing Game *****\nThis number is between 1 and 100.\nPlease enter your guess:\n");
-		scanf("%d", &i);
-		a = (i * 114514 - 1919810) % 100;
+		a = rand() % 100;
 		while (1)
 		{
+			scanf("%d", &i);
 			if (a < i)printf("It's bigger.\n");
 			else if (a > i)printf("It's smaller.\n");
 			else
@@ -19,7 +22,6 @@ void main()
 				printf("Your guess is right.\n");
 				break;
 			}
-			scanf("%d", &i);
 		}
 		printf("******************************\n");
 		printf("* Do you want to play again? *\n");
