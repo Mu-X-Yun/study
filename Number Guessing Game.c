e44ac09a67eb956af783a7e3d150a4ad9123c453
Mu-X-Yun <111125488+Mu-X-Yun@users.noreply.github.com>
2022-10-26 14:05:44 +0800

    Add files via upload

---
+++ Number Guessing Game.c
@@ -0,0 +1,30 @@
+//Ð¡ÓÎÏ·£º²ÂÊý×Ö
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+void main()
+{
+	int n = 1;
+	while (n == 1)
+	{
+		int i = 0, a = 0;
+		printf("\n***** Numnber Guessing Game *****\nThis number is between 1 and 100.\nPlease enter your guess:\n");
+		scanf("%d", &i);
+		a = (i * 114514 - 1919810) % 100;
+		while (1)
+		{
+			if (a < i)printf("It's bigger.\n");
+			else if (a > i)printf("It's smaller.\n");
+			else
+			{
+				printf("Your guess is right.\n");
+				break;
+			}
+			scanf("%d", &i);
+		}
+		printf("******************************\n");
+		printf("* Do you want to play again? *\n");
+		printf("*      Yes(1)     No(0)      *\n");
+		printf("******************************\n");
+		scanf("%d", &n);
+	}
+}
