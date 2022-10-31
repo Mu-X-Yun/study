30a5c30d4eb247369cfe73ea33cf81c71992fbec
Mu-X-Yun <111125488+Mu-X-Yun@users.noreply.github.com>
2022-11-01 00:18:06 +0800

    Update game.h

--- game.h
+++ game.h
@@ -2,7 +2,9 @@
 #define C 3
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 void ini(char arr[R][C], int r, int c);
 void print(char arr[R][C], int r, int c);
 void pm(char arr[R][C], int r, int c);
 void cm(char arr[R][C], int r, int c);
+int judge(char arr[R][C], int r, int c, char m);
