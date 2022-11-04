509339aeed44b40f679571db0008c0b12d6c446e
Mu-X-Yun <111125488+Mu-X-Yun@users.noreply.github.com>
2022-11-04 17:07:12 +0800

    Rename Exercise 6-2.c to Exercise of textbook/6/2.c

--- Exercise of textbook/6/2.c
+++ Exercise of textbook/6/2.c
@@ -1,5 +1,5 @@
-//����ĳ�̲��ϵĵ����µĵڶ��⡣
-//�����Զ��庯������ʽ���ʵ�֣���s=m!+n!+k!������m��n��k�Ӽ��̼��루ֵ��С��7����
+//这是某教材上的第六章的第二题。
+//请用自定义函数的形式编程实现，求s=m!+n!+k!，其中m、n、k从键盘键入（值均小于7）。
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 int Factorial(int x)
@@ -11,8 +11,8 @@ int Factorial(int x)
 void main()
 {
 	int m = 0, n = 0, k = 0, s = 0;
-	printf("������m,n,k��");
+	printf("请输入m,n,k：");
 	scanf("%d %d %d", &m, &n, &k);
 	s = Factorial(m) + Factorial(n) + Factorial(k);
-	printf("���s��%d", s);
+	printf("输出s：%d", s);
 }
