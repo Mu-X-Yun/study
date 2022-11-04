7b153ac7a6123a8d36c94b0c17b9b9dc094e2fa3
Mu-X-Yun <111125488+Mu-X-Yun@users.noreply.github.com>
2022-11-04 16:55:24 +0800

    Add files via upload

---
+++ Game/Number Guessing Gamee/Number Guessing Game.c
@@ -0,0 +1,32 @@
+//Ð¡ÓÎÏ·£º²ÂÊý×Ö
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void main()
+{
+	int n = 1;
+	srand((unsigned int)time(NULL));
+	while (n == 1)
+	{
+		int i = 0, a = 0;
+		printf("\n***** Numnber Guessing Game *****\nThis number is between 1 and 100.\nPlease enter your guess:\n");
+		a = rand() % 100;
+		while (1)
+		{
+			scanf("%d", &i);
+			if (a < i)printf("It's bigger.\n");
+			else if (a > i)printf("It's smaller.\n");
+			else
+			{
+				printf("Your guess is right.\n");
+				break;
+			}
+		}
+		printf("******************************\n");
+		printf("* Do you want to play again? *\n");
+		printf("*      Yes(1)     No(0)      *\n");
+		printf("******************************\n");
+		scanf("%d", &n);
+	}
+}
