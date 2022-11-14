6a4f0d1860c6fd5f60017b8b8a0c73d82bed528a
Mu-X-Yun <111125488+Mu-X-Yun@users.noreply.github.com>
2022-11-14 15:13:09 +0800

    Update 6.c

--- Experiment/4/6.c
+++ Experiment/4/6.c
@@ -1,17 +1,17 @@
-//����һ��ʵ���⡣
-//��д��������ĳ��ѧ���ĵ��Ƴɼ����������Ӧ�ĵȼ���׼��
+//这是一道实验题。
+//编写程序，输入某个学生的单科成绩，输出他对应的等级标准。
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 int main()
 {
 	int x = 0;
-	printf("�����뵥�Ƴɼ���");
+	printf("请输入单科成绩：");
 	scanf("%d", &x);
-	printf("�ȼ�Ϊ��");
+	printf("等级为：");
 	if (x >= 90)printf("Excellent");
-	if (x >= 80 && x < 90)printf("Good");
-	if (x >= 70 && x < 80)printf("Middle");
-	if (x >= 60 && x < 70)printf("Pass");
-	if (x < 60)printf("Fail");
+	else if (x >= 80)printf("Good");
+	else if (x >= 70)printf("Middle");
+	else if (x >= 60)printf("Pass");
+	else printf("Fail");
 	return 0;
 }
