0bb300640e2cfe7e0878fc077de65b887f8e56b3
Mu-X-Yun <111125488+Mu-X-Yun@users.noreply.github.com>
2022-11-18 23:46:46 +0800

    Add files via upload

---
+++ Experiment/5/3.1.c
@@ -0,0 +1,14 @@
+//这是一道实验题。
+//编写程序，计算S=1-1/2+1/3-1/4+1/5-...-1/100。
+#include <stdio.h>
+int main()
+{
+	float S = 0, sign = 1, i = 0;
+	for (i = 1; i <= 100; i++)
+	{
+		S += sign * 1 / i;
+		sign = -sign;
+	}
+	printf("%f", S);
+	return 0;
+}
