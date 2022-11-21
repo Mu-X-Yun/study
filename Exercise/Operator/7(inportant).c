23e75d88d0bb37fc95d94a4ecd64f4d9cd13cbfa
Mu-X-Yun <111125488+Mu-X-Yun@users.noreply.github.com>
2022-11-21 08:45:23 +0800

    Add files via upload

---
+++ Exercise/Operator/7(inportant).c
@@ -0,0 +1,16 @@
+//这是一道操作符的练习题。
+//不允许创建临时变量，交换两个整数的内容。
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int x, y, i;
+	printf("请输入整数x,y：");
+	scanf("%d%d", &x, &y);
+	printf("互换前x=%d，y=%d\n", x, y);
+	x += y;
+	y = x - y;
+	x = x - y;
+	printf("互换后x=%d，y=%d", x, y);
+	return 0;
+}
