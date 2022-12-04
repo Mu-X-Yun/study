4a10e4d9bcb2714e5f7c750b03ea2ba8881ff5f3
Mu-X-Yun <111125488+Mu-X-Yun@users.noreply.github.com>
2022-12-04 13:36:23 +0800

    Add files via upload

---
+++ ICPC/D.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    double Q = 365.24, M = 360, N = 389.11, P = 0, T = 0;
+    printf("%.2lf\\", P = (N - M) / M * Q);
+    printf("%.2lf", T = P * M / N);
+    return 0;
+}
